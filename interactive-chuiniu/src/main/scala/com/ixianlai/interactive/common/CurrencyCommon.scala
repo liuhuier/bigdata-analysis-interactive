f639a14bfadf03e507662e950c3b2a2f94edee11
liuhui <liuhui@xianlai-inc.com>
2019-06-01 19:07:10 +0800

    first commit

--- interactive-chuiniu/src/main/scala/com/ixianlai/interactive/common/CurrencyCommon.scala
+++ interactive-chuiniu/src/main/scala/com/ixianlai/interactive/common/CurrencyCommon.scala
@@ -5,7 +5,10 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object CurrencyCommon {
     def selectOdsBrand(spark: SparkSession)(ods_db_usercenter_hird_brand_bind: String,
                                             edw_server_loginlogout: String) = {
-        val frame: DataFrame = spark.read.table(ods_db_usercenter_hird_brand_bind)
-        
+        // 吹牛用户绑定
+        spark.read.table(ods_db_usercenter_hird_brand_bind).createOrReplaceTempView("userOds")
+        // 用户登录登出
+        spark.read.table(edw_server_loginlogout).createOrReplaceTempView("userLoginLogout")
+
     }
 }
