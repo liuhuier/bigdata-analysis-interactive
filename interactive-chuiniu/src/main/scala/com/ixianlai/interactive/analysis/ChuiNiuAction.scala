d902fd227de9713b475bccbbd8e0d3b91d75db9d
liuhui <liuhui@xianlai-inc.com>
2019-06-06 16:14:17 +0800

    first commit

--- interactive-chuiniu/src/main/scala/com/ixianlai/interactive/analysis/ChuiNiuAction.scala
+++ interactive-chuiniu/src/main/scala/com/ixianlai/interactive/analysis/ChuiNiuAction.scala
@@ -1,5 +1,6 @@
 package com.ixianlai.interactive.analysis
 
+import com.ixianlai.interactive.common.CurrencyCommon
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -7,10 +8,8 @@ import org.apache.spark.sql.SparkSession
   */
 object ChuiNiuAction {
     def main(args: Array[String]): Unit = {
-        val Array(day, ods_db_usercenter_hird_brand_bind, edw_server_loginlogout, edw_gameover_user,
-        edw_gameover_app) = args
-        val spark: SparkSession = SparkSession.builder().appName(s"liuhui_ChuiNiuAction_$day").enableHiveSupport()
-                .getOrCreate()
-
+        val Array(day, ods_db_usercenter_hird_brand_bind, edw_server_loginlogout, edw_gameover_user, edw_gameover_app) = args
+        val spark: SparkSession = SparkSession.builder().appName(s"liuhui_ChuiNiuAction_$day").enableHiveSupport().getOrCreate()
+        CurrencyCommon.selectOdsBrand(spark)(day, ods_db_usercenter_hird_brand_bind, edw_server_loginlogout, edw_gameover_user, edw_gameover_app)
     }
 }
--- interactive-chuiniu/src/main/scala/com/ixianlai/interactive/common/CurrencyCommon.scala
+++ interactive-chuiniu/src/main/scala/com/ixianlai/interactive/common/CurrencyCommon.scala
@@ -2,6 +2,7 @@ package com.ixianlai.interactive.common
 
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.expressions.Window
 
 object CurrencyCommon {
     def selectOdsBrand(spark: SparkSession)(day: String,
@@ -9,26 +10,40 @@ object CurrencyCommon {
                                             edw_server_loginlogout: String,
                                             edw_gameover_user: String,
                                             edw_gameover_app: String) = {
-        // 吹牛用户绑定
-        // col("name").as("name1")
-        val temp1 = spark.read.table(ods_db_usercenter_hird_brand_bind)
-                .where(s"brand='chuiniu' and target_day=$day")
-                .select(col("xlhu_appid").as("app_id"), col("xlhu_userid").as("user_id")).distinct()
+        import spark.implicits._
+        // 每日绑定吹牛的用户
+        // app_id user_id
+        val temp1 = spark.read.table(ods_db_usercenter_hird_brand_bind).where(s"brand='chuiniu' and target_day=$day")
+                .select($"xlhu_appid".as("app_id"), $"xlhu_userid".as("user_id")).distinct()
+
         // 用户登录登出
-        val temp2 = spark.read.table(edw_server_loginlogout)
-                .where(s"target_day=$day and (login_count>0 or logout_count>0)")
-                .select(col("app_id"), col("user_id"))
-                .distinct()
-        val temp3 = temp1.join(temp2, Seq("app_id", "user_id"), "inner").select("app_id", "user_id")
-        val res1 = temp3.agg(count("*").as("all_count")).select(col(s"${day}").as("target_day"), col("all_count"))
+        // target_day app_id user_id
+        val temp2 = spark.read.table(edw_server_loginlogout).where(s"target_day=$day and (plugin_id = 0 or plugin_id is null) and (login_count>0 or logout_count>0)")
+                .select($"target_day".cast("STRING"), $"app_id", $"user_id").distinct()
+
+        // 登录用户中绑定吹牛的用户
+        // target_day app_id user_id
+        val temp3 = temp1.join(temp2, Seq("app_id", "user_id")).select(temp2.col("target_day"), temp2.col("app_id"), temp2.col("user_id"))
+
+        // 结果一,登录用户绑定吹牛的用户总数
+        val res1 = temp3.agg(count("*").as("all_count")).select($"target_day", $"all_count")
+
+        // 亲友圈和好友桌完整把数>=1的用户
         val temp4 = spark.read.table(edw_gameover_user).where(s"target_day=$day and dissloved=false and bs_play>=1")
-                .select("app_id", "user_id", "guild_id")
-        val temp5 = temp3.join(temp4, Seq("app_id", "user_id")).select("app_id", "user_id", "guild_id")
+                .select($"app_id", $"user_id", $"guild_id")
 
-        // 好友牌局结束 -- 玩家维度 datasystem.edw_gameover_user
-        spark.read.table(edw_gameover_user).createOrReplaceTempView("gameover_user")
-        // 好友牌局结束 -- app维度 datasystem.edw_gameover_app
-        spark.read.table(edw_gameover_app).createOrReplaceTempView("gameover_app")
+        // A中亲友圈和好友桌完整把数>=1的人数
+        val temp5 = temp3.join(temp4, Seq("app_id", "user_id"))
+                .select(temp3.col("target_day"), temp3.col("app_id"), temp3.col("user_id"), temp4.col("guild_id"))
+                .withColumn("new_guild_id", when($"guild_id>0", 1).otherwise(0))
 
+        // A中亲友圈和好友桌完整把数>=1的用户数
+        val res2 = temp5.where($"new_guild_id" === 1).agg(count("*").as("qinyouquan_count")).select("target_day", "qinyouquan_count")
+        val res4 = temp5.where($"new_guild_id" === 0).agg(count("*").as("haoyouzhuo_count")).select("target_day", "hzoyouzhuo_count")
+
+        val res = res1.join(res2, "target_day").join(res4, "target_day")
+                .select(res1.col("all_count"), res2.col("qinyouquan_count"), res4.col("hzoyouzhuo_count"))
+        // res.rdd.map(_.mkString("|")).saveAsTextFile("file:")
+        res.write.format("csv").save("file:///home/hadoop/liuhui")
     }
 }
